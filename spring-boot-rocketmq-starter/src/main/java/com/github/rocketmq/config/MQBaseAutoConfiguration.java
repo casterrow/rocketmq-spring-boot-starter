3b7300d6f0b0ad383aa86246f17db4727e54617a
Stephen <ste7en.liu@gmail.com>
2018-01-09 22:53:00 +0800

    source code for rocketmq

---
+++ spring-boot-rocketmq-starter/src/main/java/com/github/rocketmq/config/MQBaseAutoConfiguration.java
@@ -0,0 +1,37 @@
+package com.github.rocketmq.config;
+
+import com.maihaoche.starter.mq.annotation.EnableMQConfiguration;
+import com.maihaoche.starter.mq.base.AbstractMQProducer;
+import com.maihaoche.starter.mq.base.AbstractMQPushConsumer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yipin on 2017/6/28.
+ * RocketMQ配置文件
+ */
+@Configuration
+@ConditionalOnBean(annotation = EnableMQConfiguration.class)
+@AutoConfigureAfter({AbstractMQProducer.class, AbstractMQPushConsumer.class})
+@EnableConfigurationProperties(MQProperties.class)
+public class MQBaseAutoConfiguration implements ApplicationContextAware {
+    @Autowired
+    protected MQProperties mqProperties;
+    protected ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = (ConfigurableApplicationContext) applicationContext;
+    }
+
+    void registerBean(String beanName, Object bean) {
+        applicationContext.getBeanFactory().registerSingleton(beanName, bean);
+    }
+}
---
+++ spring-boot-rocketmq-starter/src/main/java/com/github/rocketmq/config/MQProperties.java
@@ -0,0 +1,29 @@
+package com.github.rocketmq.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by yipin on 2017/6/28.
+ * RocketMQ的配置参数
+ */
+@Data
+@ConfigurationProperties(prefix = "rocketmq")
+public class MQProperties {
+    /**
+     * config name server address
+     */
+    private String nameServerAddress;
+    /**
+     * config producer group , default to DPG+RANDOM UUID like DPG-fads-3143-123d-1111
+     */
+    private String producerGroup;
+    /**
+     * config send message timeout
+     */
+    private Integer sendMsgTimeout = 3000;
+    /**
+     * switch of trace message consumer: send message consumer info to topic: MQ_TRACE_DATA
+     */
+    private Boolean traceEnabled = Boolean.FALSE;
+}
